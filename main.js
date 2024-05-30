7e9f020b461d264105e21cf6580b70edc641c719
jonnhyOrtega <jonnhyortega@gmail.com>
2024-05-29 21:17:07 -0300

    cambios varios

--- main.js
+++ main.js
@@ -1,20 +1,187 @@
-const mainMovies = [
-  "joker",
-  "matilda",
-  "jurassic",
-  "top gun",
-  "django",
-  "house of wax",
-  "disturbia",
-  "simpatia",
-  "shrek",
-  "batman",
-];
 
-const recommendedMovies = [
+export const arrayAll = [
+  "Gladiator",
+  "Avengers: Infinity War",
+  "Avengers: Endgame",
+  "Top Gun",
+  "Die Hard",
+  "Mad Max: Fury Road",
+  "John Wick",
+  "The Dark Knight",
+  "Inception",
+  "Mission: Impossible - Fallout",
+  "Interstellar",
+  "The Matrix",
+  "Blade Runner 2049",
+  "Back to the Future",
+  "Star Wars: A New Hope",
+  "Star Trek",
+  "The Terminator",
+  "Jurassic Park",
+  "E.T. the Extra-Terrestrial",
+  "The Avengers",
+  "Superbad",
+  "The Hangover",
+  "Step Brothers",
+  "Bridesmaids",
+  "Dumb and Dumber",
+  "Tropic Thunder",
+  "Zoolander",
+  "Old School",
+  "Hot Fuzz",
+  "The Shawshank Redemption",
+  "Forrest Gump",
+  "The Godfather",
+  "The Dark Knight",
+  "Fight Club",
+  "Pulp Fiction",
+  "Schindler's List",
+  "The Green Mile",
+  "12 Angry Men",
+  "The Pianist",
+  "Titanic",
+  "Pride and Prejudice",
+  "The Notebook",
+  "La La Land",
+  "Romeo + Juliet",
+  "A Walk to Remember",
+  "Crazy Rich Asians",
+  "The Fault in Our Stars",
+  "To All the Boys I've Loved Before",
+  "Pretty Woman",
+  "Casablanca",
+  "The Good, the Bad and the Ugly",
+  "Unforgiven",
+  "Django Unchained",
+  "The Magnificent Seven",
+  "True Grit",
+  "Once Upon a Time in the West",
+  "Butch Cassidy and the Sundance Kid",
+  "3:10 to Yuma",
+  "The Revenant",
+  "High Noon",
+  "The Exorcist",
+  "A Nightmare on Elm Street",
+  "The Conjuring",
+  "It",
+  "Hereditary",
+  "Halloween",
+  "The Shining",
+  "The Ring",
+  "Paranormal Activity",
+  "Saw",
+  "Se7en",
+  "Gone Girl",
+  "Fight Club",
+  "The Silence of the Lambs",
+  "Zodiac",
+  "Shutter Island",
+  "Memento",
+  "Prisoners",
+  "Nightcrawler",
+  "Black Swan",
+  "No Country for Old Men",
+];
+export const arrayAccion = [
+  "Gladiator",
+  "Avengers: Infinity War",
+  "Avengers: Endgame",
+  "Top Gun",
+  "Die Hard",
+  "Mad Max: Fury Road",
+  "John Wick",
+  "The Dark Knight",
+  "Inception",
+  "Mission: Impossible - Fallout",
+];
+export const arrayCienciaFiccion = [
+  "Interstellar",
+  "The Matrix",
+  "Blade Runner 2049",
+  "Back to the Future",
+  "Star Wars: A New Hope",
+  "Star Trek",
+  "The Terminator",
+  "Jurassic Park",
+  "E.T. the Extra-Terrestrial",
+  "The Avengers",
+];
+export const arrayComedia = [
+  "Superbad",
+  "The Hangover",
+  "Step Brothers",
+  "Anchorman",
+  "Bridesmaids",
+  "Dumb and Dumber",
+  "Tropic Thunder",
+  "Zoolander",
+  "Old School",
+  "Hot Fuzz",
+];
+export const arrayDrama = [
+  "The Shawshank Redemption",
+  "Forrest Gump",
+  "The Godfather",
+  "The Dark Knight",
+  "Fight Club",
+  "Pulp Fiction",
+  "Schindler's List",
+  "The Green Mile",
+  "12 Angry Men",
+  "The Pianist",
+];
+export const arrayRomance = [
+  "Titanic",
+  "Pride and Prejudice",
+  "The Notebook",
+  "La La Land",
+  "Romeo + Juliet",
+  "A Walk to Remember",
+  "Crazy Rich Asians",
+  "The Fault in Our Stars",
+  "To All the Boys I've Loved Before",
+  "Pretty Woman",
+];
+export const arrayWestern = [
+  "The Good, the Bad and the Ugly",
+  "Unforgiven",
+  "Django Unchained",
+  "The Magnificent Seven",
+  "True Grit",
+  "Once Upon a Time in the West",
+  "Butch Cassidy and the Sundance Kid",
+  "3:10 to Yuma",
+  "The Revenant",
+  "High Noon",
+];
+export const arrayTerror = [
+  "The Exorcist",
+  "A Nightmare on Elm Street",
+  "The Conjuring",
+  "It",
+  "Hereditary",
+  "Halloween",
+  "The Shining",
+  "The Ring",
+  "Paranormal Activity",
+  "Saw",
+];
+export const arrayThriller = [
+  "Se7en",
+  "Gone Girl",
+  "Fight Club",
+  "The Silence of the Lambs",
+  "Zodiac",
+  "Shutter Island",
+  "Memento",
+  "Prisoners",
+  "Nightcrawler",
+  "Black Swan",
+  "No Country for Old Men",
+];
+export const recommendedMovies = [
   "creed",
   "rocky",
-  "interestelar",
   "moonfall",
   "hooligans",
   "blade",
@@ -23,8 +190,7 @@ const recommendedMovies = [
   "reservoir dogs",
   "flubber",
 ];
-
-const galleryPostersArray = [
+export const galleryPostersArray = [
   ["harry potter", "toy story", "robocop", "small soldiers"],
   ["titanic", "john wick", "benjamin button", "jaws"],
   [
@@ -40,17 +206,16 @@ const galleryPostersArray = [
     "Interstellar",
   ],
 ];
-
 let containerMainMovies = document.querySelector("#list-main-movies");
 let containerRecommendedMovies = document.querySelector("#list-recommended");
 let cart = document.querySelector("#dropdown-cart");
 let galleryPoster = document.querySelector("#gallery-poster");
 let searchButton = document.querySelector("#search-button");
-let nextGallery = document.querySelector("#next-gallery");
-let previousGallery = document.querySelector("#previous-gallery");
 let numberProducts = document.querySelector("#number-products");
 let arrayCart = JSON.parse(localStorage.getItem("arrayCart")) || [];
-let arrayProfiles = JSON.parse(localStorage.getItem("arrayCart")) || [];
+let arrayProfiles = JSON.parse(localStorage.getItem("arrayProfiles")) || [];
+let filterButton = document.querySelector("#filter-button");
+
 // FUNCTION TO GET MOVIE DATA IN API
 async function obtenerDatos(string) {
   const URL = "https://www.omdbapi.com/?apikey=712dcc23&t=";
@@ -74,14 +239,13 @@ async function obtenerDatos(string) {
     return null;
   }
 }
-
 // FUNCTION TO CREATE CARD TEMPLATE WITH DATA FROM API
 function createCardTemplate(object) {
   return `
     <div id="movie">
         <img src="${object.Poster}" alt="${object.Title}">
         <span>${object.Title} - ${object.Year}</span>
-        <span>Puntuación: ${object.imdbRating}</span>
+        <span>Puntuación: ${object.imdbRating} - ${renderStarsRate(object.imdbRating)}</span>
         <button id="${object.Title}" class="btn-add"><i class="fa-solid fa-circle-plus"></i></button>
     </div>
   `;
@@ -92,6 +256,7 @@ async function renderMovies(place, array) {
   for (const movie of array) {
     try {
       const movieToRender = await obtenerDatos(movie);
+      
       if (movieToRender) {
         place.innerHTML += createCardTemplate(movieToRender);
       } else {
@@ -108,20 +273,23 @@ async function renderMovies(place, array) {
 const saveToLocalStorage = () => {
   localStorage.setItem("arrayCart", JSON.stringify(arrayCart));
 };
+const saveToLocalStorageProfiles = () => {
+  localStorage.setItem("arrayProfiles", JSON.stringify(arrayProfiles));
+};
 
 // FUNCTION TO ADD MOVIE TO ARRAY CART
 function addEventListenersToButtons() {
   const btnAdd = document.querySelectorAll(".btn-add");
   btnAdd.forEach((btn) => {
     btn.addEventListener("click", (e) => {
-      console.log(e.target.id)
+      console.log(e.target.id);
       const movieTitle = e.currentTarget.id;
       obtenerDatos(movieTitle).then((data) => {
-        console.log(data)
+        console.log(data);
         if (!data) return;
         let existingMovie = arrayCart.find(
           (movie) => movie.Title === data.Title
-        )
+        );
         if (!existingMovie) {
           let price = getRandomInt(2000, 10000);
           data.price = price;
@@ -130,7 +298,7 @@ function addEventListenersToButtons() {
           saveToLocalStorage();
           console.log(`Película ${data.Title} agregada al carrito.`);
         } else {
-          console.log("La pelicula ya se encuentra en el carrito.")
+          console.log("La pelicula ya se encuentra en el carrito.");
           // existingMovie.cantidad + 1; // Increment the quantity
           // console.log(
           // );
@@ -175,6 +343,7 @@ function renderArrayCart() {
       total += totalPrice;
       numberProducts.textContent = totalProductsCart;
     });
+
     cart.innerHTML += `
     <div id="container-total">
       <p>${" $" + total}</p>
@@ -182,6 +351,7 @@ function renderArrayCart() {
       <button id="delete-all"><i class="fa-solid fa-trash-can"></i></button>
     </div>
       `;
+      
     deleteAllProducts();
     function deleteAllProducts() {
       document.querySelector("#delete-all").addEventListener("click", () => {
@@ -208,9 +378,9 @@ function renderArrayCart() {
         movie.cantidad += 1;
         saveToLocalStorage();
         renderArrayCart();
-        console.log( `Cantidad de película "${movie.Title}" actualizada en el carrito.`
-      )
-
+        console.log(
+          `Cantidad de película "${movie.Title}" actualizada en el carrito.`
+        );
       }
     });
   });
@@ -237,6 +407,17 @@ function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// HELPER FUNCTIONS TO VALID DATA
+function containNumbers(texto) {
+  let regex = /\d/;
+  return regex.test(texto);
+}
+
+function isEmail(texto) {
+  let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(texto);
 }
 
 // FUNCTION TO RENDER POSTER IN HERO SECTION
@@ -293,7 +474,7 @@ function addEventListenersToPosters() {
         searchMoviesHTML.innerHTML = `
       ${createCardTemplate(data)}
       `;
-      addEventListenersToButtons()
+        addEventListenersToButtons();
         section.scrollIntoView({ behavior: "smooth" });
       });
     });
@@ -311,13 +492,87 @@ async function searchMovie() {
         searchMoviesHTML.innerHTML = `<p>La pelicula que busco no ha sido encontrada, por favor vuelva a intentarlo.</p>
         `;
       }
+      if (data.Poster === "N/A") {
+        searchMoviesHTML.innerHTML = `
+        <span > Nada para mostrar con ese nombre => ${data.Title} </span>  
+      `;
+        return ;
+      }
       searchMoviesHTML.innerHTML = `
         ${createCardTemplate(data)}
       `;
-      addEventListenersToButtons()
+      addEventListenersToButtons();
     });
   });
 }
+
+// FUNCTION TO RENDER STARS IN RATED
+function renderStarsRate(rated) {
+  switch (true) {
+    case (rated >= 0 && rated <= 1):
+      return `⭐`;
+    case (rated > 1 && rated <= 3):
+      return `⭐⭐`;
+    case (rated > 3 && rated <= 7): 
+      return `⭐⭐⭐`;
+    case (rated >7 && rated <= 9):
+      return `⭐⭐⭐⭐`;
+    case (rated > 9):
+      return `⭐⭐⭐⭐⭐`;
+    case (rated == "N/A"):
+      return `❗`;
+      default:
+      return `❗`;
+  }
+}
+
+// FUNCTION TO FILTER MOVIES FOR CATEGORIES
+filterButton.addEventListener("click", () => {
+  let genreSelect = document.querySelector("#genre-select");
+  console.log(genreSelect);
+  let selectedValue = genreSelect.value;
+  switch (selectedValue) {
+    case "All":
+      containerMainMovies.innerHTML = "";
+      renderMovies(containerMainMovies, arrayAll);
+      break;
+    case "Accion":
+      containerMainMovies.innerHTML = "";
+      renderMovies(containerMainMovies, arrayAccion);
+      break;
+    case "Ciencia Ficcion":
+      containerMainMovies.innerHTML = "";
+      renderMovies(containerMainMovies, arrayCienciaFiccion);
+      break;
+    case "Comedia":
+      containerMainMovies.innerHTML = "";
+      renderMovies(containerMainMovies, arrayComedia);
+      break;
+    case "Drama":
+      containerMainMovies.innerHTML = "";
+      renderMovies(containerMainMovies, arrayDrama);
+      break;
+    case "Romance":
+      containerMainMovies.innerHTML = "";
+      renderMovies(containerMainMovies, arrayRomance);
+      break;
+    case "Western":
+      containerMainMovies.innerHTML = "";
+      renderMovies(containerMainMovies, arrayWestern);
+      break;
+    case "Terror":
+      containerMainMovies.innerHTML = "";
+      renderMovies(containerMainMovies, arrayTerror);
+      break;
+    case "Thriller":
+      containerMainMovies.innerHTML = "";
+      renderMovies(containerMainMovies, arrayThriller);
+      break;
+    default:
+      containerMainMovies.innerHTML = "";
+      console.log("Género no reconocido");
+  }
+});
 
 // FUNCTION TO FORM CONTACT
 let formContact = document.querySelector("#contact-form");
@@ -333,15 +588,25 @@ formContact.addEventListener("submit", (e) => {
     message: message.value,
   };
 
-  console.log(profile);
+  if(
+    containNumbers(profile.name) == false &&
+    isEmail(profile.email) == true){
+    arrayProfiles.push(profile)
+    saveToLocalStorageProfiles()
+
+  }if(containNumbers(profile.name) == false){
+    console.log("Escriba un nombre correcto sin numeros")
+  }if(isEmail(profile.email) == false){
+    console.log(`${profile.email} no es un dominio correcto` )
+  }
+  console.log("No se cumplio ninguna condicion")
+  return
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  renderMovies(containerMainMovies, mainMovies);
   renderMovies(containerRecommendedMovies, recommendedMovies);
   renderArrayCart();
   renderGallery(galleryPostersArray);
+  renderMovies(containerMainMovies, arrayAll);
   searchMovie();
 });
-
-console.log(arrayCart);
